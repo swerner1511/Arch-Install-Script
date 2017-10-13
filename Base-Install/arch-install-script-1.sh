6f74f5c679a845b665703ba3e6d35f63ac862155
swerner1511 <s.werner_1990@gmx.net>
2017-10-13 14:04:03 +0200

    Update arch-install-script-1.sh

--- Base-Install/arch-install-script-1.sh
+++ Base-Install/arch-install-script-1.sh
@@ -55,7 +55,7 @@ echo "# Installing & configuring Arch Linux #"
 echo "#######################################"
 echo "#"
 echo "#Bootstrapping Arch..."
-pacstrap /mnt base base-devel wpa_supplicant dialog
+pacstrap /mnt base base-devel wpa_supplicant dialog intel-ucode
 
 #download the second script for chroot part
 wget https://raw.githubusercontent.com/swerner1511/Arch-Install-Script/master/Base-Install/arch-install-script-2.sh
