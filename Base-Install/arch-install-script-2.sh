b5c3b75130d4650ecaf5e428b44f5ca7f2aa0104
swerner1511 <s.werner_1990@gmx.net>
2017-10-13 14:00:21 +0200

    clean up and more
    create user added
    change visudo
    set root pw added

--- Base-Install/arch-install-script-2.sh
+++ Base-Install/arch-install-script-2.sh
@@ -22,38 +22,12 @@ ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 echo "#"
 echo "#Generate locales"
 sed -ie 's/#de_DE/de_DE/g' /etc/locale.gen
-sed -ie 's/#en_US/en_US/g' /etc/locale.gen
+#sed -ie 's/#en_US/en_US/g' /etc/locale.gen
 locale-gen
 echo "#"
 echo "#Set default locales..."
-echo "LANG=de_DE.UTF-8
-LC_CTYPE="de_DE.UTF-8"
-LC_NUMERIC="de_DE.UTF-8"
-LC_TIME="de_DE.UTF-8"
-LC_COLLATE="de_DE.UTF-8"
-LC_MONETARY="de_DE.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-LC_PAPER="de_DE.UTF-8"
-LC_NAME="de_DE.UTF-8"
-LC_ADDRESS="de_DE.UTF-8"
-LC_TELEPHONE="de_DE.UTF-8"
-LC_MEASUREMENT="de_DE.UTF-8"
-LC_IDENTIFICATION="de_DE.UTF-8"
-LC_ALL=" >> /etc/locale.conf
-# echo "LANG=de_DE.UTF-8
-# LC_CTYPE="de_DE.UTF-8"
-# LC_NUMERIC="de_DE.UTF-8"
-# LC_TIME="de_DE.UTF-8"
-# LC_COLLATE="de_DE.UTF-8"
-# LC_MONETARY="de_DE.UTF-8"
-# LC_MESSAGES="de_DE.UTF-8"
-# LC_PAPER="de_DE.UTF-8"
-# LC_NAME="de_DE.UTF-8"
-# LC_ADDRESS="de_DE.UTF-8"
-# LC_TELEPHONE="de_DE.UTF-8"
-# LC_MEASUREMENT="de_DE.UTF-8"
-# LC_IDENTIFICATION="de_DE.UTF-8"
-# LC_ALL=" >> /etc/locale.conf
+echo "LANG=de_DE.UTF-8" > /etc/locale.conf
+echo "LANGUAGE=de_DE" >> /etc/locale.conf
 echo "#"
 echo "#Set console keymap..."
 echo "KEYMAP=de-latin1-nodeadkeys" > /etc/vconsole.conf
@@ -71,7 +45,7 @@ echo "#Configuring Grub..."
 # TODO : shorten the UUID-Part ^^
 # grep only UUID
 cryptsetup luksDump /dev/sda2 | grep UUID > tempUUID.txt
-# renove UUID from string
+# remove "UUID:" from string
 sed -ie 's/UUID:/ /g' tempUUID.txt
 # remove spaces and tabs from string
 sed -ie 's/^[ \t]*//' tempUUID.txt
@@ -121,7 +95,25 @@ echo "#"
 echo "###########################"
 echo "# remove unwanted pkgs... #"
 echo "###########################"
-pacman -R baobab empathy epiphany totem accerciser aisleriot anjuta atomix five-or-more four-in-a-row gnome-2048 gnome-chess gnome-klotski gnome-mahjongg gnome-mines gnome-music gnome-nibbles gnome-robots gnome-sudoku gnome-taquin gnome-tetravex hitori iagno lightsoff orca quadrapassel swell-foop tali
+pacman -R --noconfirm baobab empathy epiphany totem accerciser aisleriot anjuta atomix five-or-more four-in-a-row gnome-2048 gnome-chess gnome-klotski gnome-mahjongg gnome-mines gnome-music gnome-nibbles gnome-robots gnome-sudoku gnome-taquin gnome-tetravex hitori iagno lightsoff orca quadrapassel swell-foop tali
+echo "#"
+echo "###########################"
+echo "# create useraccount... #"
+echo "###########################"
+echo "Enter your username..."
+read USERNAME
+useradd -m -g users -G wheel,audio,video -s /bin/bash $USERNAME
+passwd $USERNAME
+echo "User created..."
+echo "Don't forget to EDITOR=nano visudo..."
+echo "Press any key..."
+read
+EDITOR=nano visudo
+echo ""
+echo "###########################"
+echo "# set root password... #"
+echo "###########################"
+passwd
 echo "#"
 echo "#Exit from chroot environment..."
 exit
